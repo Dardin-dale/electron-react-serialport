ddec099cb2a9c4a22e086792c781cce6654d5263
Logan Crecraft <logandalec@gmail.com>
2020-06-23 23:25:16 -0700

    build chain working

---
+++ src/Components/Dropdown/index.js
@@ -0,0 +1,17 @@
+//Here is a Combobox react example to select devices
+
+import React from 'react';
+
+class Dropdown extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          listOpen: false,
+          headerTitle: this.props.title
+        }
+    }
+
+    
+}
+
+export default Dropdown;
