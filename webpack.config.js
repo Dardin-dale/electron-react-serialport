23bc489fd9c4b2e828a2df643f5bbd99b2825618
Logan Crecraft <logandalec@gmail.com>
2020-08-17 14:00:25 -0700

    working on re-org

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  "target":"electron-main",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './public',
+    hot: true,
+    port: 8082
+  },
+  externals: {serialport: true}
+};
